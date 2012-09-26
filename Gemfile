0cedd48b375ad48e9ae3b6644d1db7d904325167
Neal Clark <nclark@thrownproject.com>
2012-09-25 22:17:01 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source :rubygems
+
+gem 'sqlite3', '= 1.3.6'
+gem 'rails',   '= 3.2.8'
+
+gem 'carrierwave', '~> 0.6.2'
+gem 'fog',         '~> 1.6.0'
+gem 'mini_magick', '~> 3.4'
+
+group :development, :test do
+  gem 'rspec-rails', '~> 2.11.0' # ugh
+end
+
+group :test do
+  gem 'factory_girl_rails', '~> 4.1.0'
+  gem 'vcr',                '~> 2.2.5'
+  gem 'webmock',            '~> 1.8.10'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebmockDemo::Application.config.secret_token = 'c78257352a2f7f2b0a5c310e8cc986d8efb3f7b7a333bbb8f8ee7d57d95cf516b20a7e2e1b7bb4ef7bdfa7d42628529159e17c56db7f643d16a9512b9bd7e875'
